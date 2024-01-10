0d64f88c0cc4bb1c7dae7c89125da1d14d820dd8
DevSankale <sankaledev@gmail.com>
2024-01-10 22:05:46 +0300

    Todo scripts and styles updates

--- todo-list.js
+++ todo-list.js
@@ -14,26 +14,36 @@ function renderTodoList() {
 
 
   let todoListHTML = '';
-  for (i = 0; i < todoList.length; i++) {
-    const todoObject = todoList[i]
+
+  todoList.forEach((todoObject, index) => {
+
     const name = todoObject.name;
     const dueDate = todoObject.dueDate;
     const html = `
     <div>${name}</div>
     <div>${dueDate}</div>
-    <button onclick="
-    todoList.splice(${i},1);
-    renderTodoList();
-    " class="delete-todo-button">Delete</button>
+    <button class="delete-todo-button js-delete-todo-button">Delete</button>
     `;
     todoListHTML += html;
-  };
-
+  });
   document.querySelector('.js-todo-list')
     .innerHTML = todoListHTML;
 
+  document.querySelectorAll('.js-delete-todo-button').forEach((deleteButton, index) => {
+    deleteButton.addEventListener('click', () => {
+      todoList.splice(index, 1);
+      renderTodoList();
+    });
+  });
+
 
 }
+
+document.querySelector('.js-add-todo-button').addEventListener('click', () => {
+  addTodo();
+})
+
+
 function addTodo() {
   const inputElement = document.querySelector('.inputButton-js');
   const name = inputElement.value;
